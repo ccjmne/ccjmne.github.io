c5bf28a10a526af7f0669dd208ab80119d6b9025
ccjmne <ccjmne@gmail.com>
2020-02-24 23:40:02 +0100

    Refine global css styling injection mechanism

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,19 @@
-import path from 'path';
+import { resolve } from 'path';
+import { Configuration } from 'webpack';
 
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin, { MinifyOptions } from 'html-webpack-plugin';
 import ImageminPlugin from 'imagemin-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
-import { Configuration } from 'webpack';
-// import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import StyleExtHtmlWebpackPlugin from 'style-ext-html-webpack-plugin';
 
-const HTML_MINIFY_OPTS: HtmlWebpackPlugin.Options = {
+import { author, description, keywords, repository } from './package.json';
+
+const HTML_MINIFY_OPTS: MinifyOptions = {
   removeComments: true,
-  removeCommentsFromCDATA: true,
-  removeCDATASectionsFromCDATA: true,
   collapseWhitespace: true,
   conservativeCollapse: true,
   removeAttributeQuotes: true,
@@ -21,15 +22,16 @@ const HTML_MINIFY_OPTS: HtmlWebpackPlugin.Options = {
   minifyJS: true,
   minifyCSS: true,
   removeScriptTypeAttributes: true,
-  removeStyleTypeAttributes: true,
 };
 
-const assets = path.resolve(__dirname, 'assets');
-const src = path.resolve(__dirname, 'src');
+const assets = resolve(__dirname, 'assets');
+const src = resolve(__dirname, 'src');
+const sass = resolve(__dirname, 'sass');
 
 export default (_env: string, argv: { mode?: 'production' | 'development', analyze?: boolean }): Configuration => ({
   entry: {
-    main: path.resolve(src, 'index.ts'),
+    'main': resolve(src, 'index.ts'),
+    'global-styles': resolve(src, 'main.scss'),
   },
   devtool: (argv.mode === 'production' ? false : 'cheap-module-eval-source-map'),
   optimization: {
@@ -61,10 +63,15 @@ export default (_env: string, argv: { mode?: 'production' | 'development', analy
           attrs: ['img:src', 'img:data-src', 'video:src', 'video:data-src'],
           interpolate: true,
           root: __dirname,
-          minimize: argv.mode === 'production',
-          ...HTML_MINIFY_OPTS,
+          minimize: argv.mode === 'production' && HTML_MINIFY_OPTS,
         },
       }],
+    }, {
+      test: /(?<!\.inline)\.s?css$/,
+      enforce: 'pre',
+      use: [
+        { loader: MiniCssExtractPlugin.loader },
+      ],
     }, {
       test: /\.s?css$/,
       enforce: 'pre',
@@ -75,24 +82,19 @@ export default (_env: string, argv: { mode?: 'production' | 'development', analy
       test: /\.scss$/,
       enforce: 'pre',
       use: [
-        { loader: 'sass-loader', options: { outputStyle: 'compressed' } },
-        { loader: 'sass-resources-loader', options: { resources: './styles/commons.scss' } },
+        { loader: 'sass-loader' },
+        { loader: 'sass-resources-loader', options: { resources: resolve(sass, 'scss-inject.scss') } },
       ],
-    }, /* , {
-      test: /\.s?css$/,
-      use: [
-        { loader: MiniCssExtractPlugin.loader },
-      ],
-    } */ {
+    }, {
       test: /\.(png|jpe?g|gif|ico|webp|mp4)$/,
       use: [
-        { loader: 'url-loader', options: { limit: 2048, outputPath: 'assets/' } },
+        { loader: 'url-loader', options: { limit: 2048, outputPath: assets } },
       ],
     }, {
       test: /\.svg$/,
       issuer: /\.(html|js)$/,
       use: [
-        { loader: 'file-loader', options: { outputPath: 'assets/' } },
+        { loader: 'file-loader', options: { outputPath: assets } },
       ],
     }, {
       test: /\.svg$/,
@@ -110,19 +112,21 @@ export default (_env: string, argv: { mode?: 'production' | 'development', analy
   },
   plugins: [].concat(
     argv.mode === 'production' ? new CleanWebpackPlugin() : [],
-    new CopyWebpackPlugin([{ from: path.resolve(assets, 'favicons/'), to: 'favicons/' }]),
+    new CopyWebpackPlugin([{ from: resolve(assets, 'favicons'), to: 'favicons' }]),
     argv.analyze ? new BundleAnalyzerPlugin() : [],
-    // new MiniCssExtractPlugin({
-    //   filename: '[name].css',
-    //   chunkFilename: '[id].css',
-    // }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'index.html'),
+      template: resolve(__dirname, 'index.html'),
       chunks: ['commons', 'main', 'logo'],
       chunksSortMode: 'manual',
-      ...HTML_MINIFY_OPTS,
+      minify: HTML_MINIFY_OPTS,
+      meta: { author, description, repository, keywords: keywords.join(', ') },
     }),
     new ScriptExtHtmlWebpackPlugin({ defaultAttribute: 'defer' }),
+    new StyleExtHtmlWebpackPlugin(),
     argv.mode === 'production' ? new ImageminPlugin({}) : [],
   ),
 });
